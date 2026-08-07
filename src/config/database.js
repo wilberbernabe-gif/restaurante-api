dd2175ea8772af3d8ff3daf2afeee5bd1f3b4f8e
Wilber Bernabe <wilber.bernabe@clases.edu.sv>
2026-08-07 17:38:22 -0600

    configura conexion postgreeSQL para produccion

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "12345",
-    database: "restaurante",
-    port: 5432
-});
+const pool = new Pool(
+    process.env.DATABASE_URL
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+        : {
+            host: "localhost",
+            user: "postgres",
+            password: "12345",
+            database: "restaurante",
+            port: 5432
+        }
+);
 
 module.exports = pool;
